c0b152308b4c79500e09f9e3916684e379d6e11a
ShoOshiro <shota.0615.soccer@gmail.com>
2021-01-16 17:40:52 +0900

    refactor profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,11 @@ import {withRouter} from 'react-router';
 import {inject, observer} from 'mobx-react';
 import EditProfile from './EditProfile';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
-import {PrimaryButton} from '../common/ui-kit/index';
 import {fetchOwnTweets} from '../mobx/tweet/operations'
 import {DisplayTweets} from './'
 import Tabs from '@material-ui/core/Tabs';
@@ -39,9 +40,9 @@ class Profile extends React.Component {
 
     displayTab = (tweets) => {
         if(this.state.selectTab === 0){
-            return <DisplayTweets tweets={tweets} replyList={this.props.TweetStore.replyList}/> 
+            return <DisplayTweets tweets={tweets} replyList={this.props.TweetStore.replyList} userData={this.props.UserStore}/> 
         }else if(this.state.selectTab === 1){
-            return <h2>'Good' will be displayed. It is under construction.</h2>
+            return <p>'Good' will be displayed. It is under construction.</p>
         }
     } 
 
@@ -52,25 +53,28 @@ class Profile extends React.Component {
         return (
             <div className="container">
                 <Card>
-                    <IconButton style={{cursor:'pointer', float:'right', marginTop: '5px', marginRight: '5px', width: '20px'}} aria-label="edit profile">
-                    <SettingsIcon 
-                        onClick={this.openModal}
+                    <CardHeader
+                        avatar={
+                            <UserImage path={userData.userImageUrl} style={{width: '120px', height: '120px'}}/>
+                        }
+                        action={
+                            <IconButton style={{cursor:'pointer', float:'right', marginTop: '5px', marginRight: '5px', width: '20px'}} aria-label="edit profile">
+                                <SettingsIcon 
+                                    onClick={this.openModal}
+                                />
+                            </IconButton>}
+                        title={userData.userName}
                     />
-                    </IconButton>
-                    <UserImage path={userData.userImageUrl} style={{width: '150px', height: '150px'}}/>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {userData.userName}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {userData.bio}
-                    </Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {userData.bio}
+                        </Typography>
+                    </CardContent>
                     <EditProfile 
                         openModal = {this.state.isModalOpen}
                         onClose = {this.closeModal}
                         userData={userData}
                     /> 
-                <PrimaryButton label={'followers'} style={{marginLeft: '5px', marginRight: '5px'}}/> 0   
-                <PrimaryButton label={'following'} style={{marginLeft: '5px', marginRight: '5px'}}/> 0
                 </Card>
                 {/* TODO: 脳筋Tabを辞めてreact-tab等を用いること。 */}
                 <Tabs
@@ -79,7 +83,7 @@ class Profile extends React.Component {
                     aria-label="disabled tabs example"
                 >
                     <Tab label="Tweets" onClick={() => {this.setState({selectTab: 0})}}/>
-                    <Tab label="Likes♡" onClick={() => {this.setState({selectTab: 1})}}/>
+                    <Tab label="Likes" onClick={() => {this.setState({selectTab: 1})}}/>
                 </Tabs>
                 {this.displayTab(tweets)}    
             </div>
